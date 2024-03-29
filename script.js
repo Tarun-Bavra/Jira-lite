3b23b8e81016b93930a32cd9160637126e168876
Tarun <tarunbavra@gmail.com>
2024-03-29 07:47:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("create-task-form");
+
+form.addEventListener("submit", (e) => {            
+  e.preventDefault();
+  const taskStatus = form.status.value;
+  const taskInfo = {
+    title: form.title.value,
+    description: form.description.value,
+    developer: form.developer.value,
+    estimate: form.estimate.value,
+  };
+  console.log(taskInfo);
+});
+
+/*
+
+ <div class="task-card" title="This is a Dummy Task">
+  <h3>Chat Box</h3>
+  <div>
+    <span class="badge">3 Days</span>
+    <span class="image">S</span>
+  </div>
+</div> 
+
+*/
